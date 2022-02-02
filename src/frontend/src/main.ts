cedf1ec4075c553730c696739bdbbd6731f4ad38
ZadenRB <zaden.ruggieroboune@gmail.com>
2022-02-02 02:47:54 -0500

    Fix local development request URI

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -4,8 +4,7 @@ const app = new App({
 	target: document.body,
 	props: {
 		popularityDescription: "popularity",
-		requestURI: "http://10.110.117.218:8000/random",
-		// requestURI: "https://spotify-higher-lower-api.herokuapp.com/random",
+		requestURI: "https://spotify-higher-lower-api.herokuapp.com/random",
 	},
 });
 
